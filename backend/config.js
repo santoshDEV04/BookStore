f9abb99d6afcc1eaa1c1538c5d9a5e928e069d99
Santosh Kumar Dash <dashsantosh1333@gmail.com>
2025-10-18 20:26:24 +0530

    Delete backend/config.js
    config deleted

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://dashsantosh1333_db_user:gxiRkPgFX8GPaUE3@cluster0.1qlxbvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
